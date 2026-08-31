64a2c9c3e67166331cbc565dc93c283a52b12ec2
Piyush Yadav <2432354.piyush@gmail.com>
2026-08-31 15:04:22 +0530

    structure fixed

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,2 +1,7 @@
-// TODO: Authentication teammate implementation
-export const authService = {}
+import { authRequest } from '../modules/auth/authenticationApi.js'
+
+export const authService = {
+  login: (credentials) => authRequest('login', credentials),
+  register: (account) => authRequest('register', account),
+  getCurrentUser: (token) => authRequest('me', null, token),
+}
